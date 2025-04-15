f843db83b5584b347215914fbe4a7122905a43e5
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-04-15 10:41:46 +0800

    update widget_text run app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:tetris_flutter_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const TetrisGame());
+    await tester.pumpWidget(const TetrisApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
